6dff1bd31e0dee31a92111d24dc7804b5f447625
Ken Kenney <kenkenney@Kens-MacBook-Pro.local>
2020-05-26 17:56:20 -0400

    updates

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+#Pages app-urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+]
