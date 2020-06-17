b06845ad4b46db23bf1e918ee8a0368c8910ede0
james kenney <jkenney0501@gmail.com>
2020-06-17 14:29:33 -0400

    added environ variables

--- btre/settings.py
+++ btre/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@wnq-i&k_qsqip%62_w2&hzww#r6*l(4scn)i*%50h@_dijdi)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,8 +72,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'Apolloreed1#',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
